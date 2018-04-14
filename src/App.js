0259e9e4b0dbb678cdc695648c83c4755b7047ac
garfield <cmgarfield@gmail.com>
2018-04-14 15:40:53 +0800

    Lec.44, pass method as props of another component

--- src/App.js
+++ src/App.js
@@ -29,9 +29,16 @@ class App extends Component {
         <h1>Hi, I'm a React App.</h1>
         <p>This is really working!</p>
         <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <Person
+         name={this.state.persons[0].name}
+         age={this.state.persons[0].age}/>
+        <Person
+         name={this.state.persons[1].name}
+         age={this.state.persons[1].age}
+         click={this.switchNameHandler}>My Hobbies: Racing</Person>
+        <Person
+         name={this.state.persons[2].name}
+         age={this.state.persons[2].age} />
       </div>
     );
     // return React.createElement('div', {class: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App.'));
