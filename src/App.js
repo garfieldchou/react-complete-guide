180b53e5004d9604a131c035605fbabbe3ecff73
garfield <cmgarfield@gmail.com>
2018-04-22 12:53:45 +0800

    Revert "Lec.75, ErrorBoundary"
    This reverts commit 85b42745c56be751884a0b08ca3f45f9d6771b37.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import classes from './App.css';
-import Person from './Person/Person';
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
+import Person from './Person/Person'
 
 class App extends Component {
   state = {
@@ -51,13 +50,12 @@ class App extends Component {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <ErrorBoundary key={person.id}>
-              <Person
+            return <Person
               click={this.deletePersonHandler.bind(this, index)}
               name={person.name}
               age={person.age}
+              key={person.id}
               changed={(event) => {this.nameChangedHandler.bind(this)(event, person.id)}} />
-            </ErrorBoundary>
           })}
         </div>
       );
