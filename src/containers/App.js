0ae605f78a2f702b3004e22ad8abc8941fa5d964
garfield <cmgarfield@gmail.com>
2018-04-24 23:25:51 +0800

    Lec.89, use PureComponent to do the shallow comparison to replace shouldComponentUpdate()

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
 import classes from "./App.css";
 import Persons from "../components/Persons/Persons";
 import Cockpit from "../components/Cockpit/Cockpit";
 
-class App extends Component {
+class App extends PureComponent {
   constructor(props) {
     super(props);
     console.log('[App.js] Inside Constructor: ', props);
@@ -23,12 +23,6 @@ class App extends Component {
 
   componentDidMount() {
     console.log('[App.js] Inside componentDidMount()');
-  }
-
-  shouldComponentUpdate( nextProps, nextState ) {
-    console.log("[UPDATE App.js] Inside shouldComponentUpdate()", nextProps, nextState);
-    return nextState.persons !== this.state.persons ||
-        nextState.showPersons !== this.state.showPersons;
   }
 
   componentWillUpdate( nextProps, nextState ) {
